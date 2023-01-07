58904a0ac2ecbf60e4287040d5c45510f12eff46
Emma Bosworth <emmab3105@gmail.com>
2023-01-07 10:26:32 +0000

    work from today

--- src/App.js
+++ src/App.js
@@ -1,37 +1,57 @@
 // import logo from "./logo.svg";
 // import "./App.css";
 
+import React, { useState} from 'react';
 import Heading from "./Components/Heading";
 import InputComponent from "./Components/InputComponent";
-import TaskComponent from "./Components/TaskComponent";
+
 
 function App() {
-  const task = [
-    {
-      id: 1,
-      taskName: "task name 1",
-      deleteButton: "delete",
-      editButton: "edit",
-    },
-    {
-      id: 2,
-      taskName: "task name 2",
-      deleteButton: "delete",
-      editButton: "edit",
-    },
-    {
-      id: 3,
-      taskName: "task name 3",
-      deleteButton: "delete",
-      editButton: "edit",
-    },
-  ];
+  // const task = [
+  //   {
+  //     id: 1,
+  //     taskName: "task name 1",
+  //     deleteButton: "delete",
+  //     editButton: "edit",
+  //   },
+  //   {
+  //     id: 2,
+  //     taskName: "task name 2",
+  //     deleteButton: "delete",
+  //     editButton: "edit",
+  //   },
+  //   {
+  //     id: 3,
+  //     taskName: "task name 3",
+  //     deleteButton: "delete",
+  //     editButton: "edit",
+  //   },
+  // ];
+
+  const [task, addTask] = useState ("");
 
   return (
     <div className="App">
       <Heading />
-      <InputComponent />
-      <TaskComponent />
+      <div>
+      <div class="task-input">
+        <i className="uil-align-left"></i>
+        <input
+          className="input-task-input"
+          id="input-task-input"
+          type="text"
+          placeholder="Add new task" 
+          
+        ></input>
+        <button onClick={() => addTask(  
+        <div>
+    <span>input</span>
+    <button>delete</button>
+     <button>edit</button>
+   </div>)}> Add ToDo </button>
+      </div>
+    </div>
+    <p>{task}</p>
     </div>
   );
 }
--- src/Components/InputComponent.js
+++ src/Components/InputComponent.js
@@ -1,22 +1,19 @@
-import React from "react";
-import AddButton from "./AddButton";
+import React, { useState } from "react";
 
 const InputComponent = () => {
-  return (
-    <div>
-      <div class="task-input">
-        <i className="uil-align-left"></i>
-        <input
-          className="input-task-input"
-          id="input-task-input"
-          type="text"
-          placeholder="Add new task"
-          value=""
-        ></input>
-        <button onClick={AddButton}>Add ToDo </button>
-      </div>
-    </div>
-  );
+  const [task, addTask] = useState ("");
+
+
+  // const addtask=()=> {
+  
+  // //   <div>
+  // //   <button>delete</button>
+  // //    <button>edit</button>
+  // //  </div>
+    
+  // }
+  
+
 };
 
 export default InputComponent;
