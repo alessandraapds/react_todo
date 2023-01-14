e22f09faa4b188543df554a40565bdda836f9667
Emma Bosworth <emmab3105@gmail.com>
2023-01-14 11:20:52 +0000

    added edit function

--- src/Components/InputComponent.js
+++ src/Components/InputComponent.js
@@ -5,7 +5,9 @@ const InputComponent = () => {
   const [newItem, setNewItem] = useState("");
   const [items, setItems] = useState([]);
 
-  function addItem() {
+
+  function addItem(e) {
+    e.preventDefault();
     const item = {
       id: Math.floor(Math.random() * 1000),
       value: newItem,
@@ -20,9 +22,71 @@ const InputComponent = () => {
     setItems(newArray);
   }
 
+  //Edit an Item//
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState({});
+
+  function handleEditInputChange(e) {
+    console.log(e, "console test e")
+    e.preventDefault();
+    setCurrentTodo({...currentTodo, value:e.target.value });
+  }
+
+  console.log(currentTodo.value, "test");
+
+function handleEditClick(todo) {
+  setIsEditing(true);
+  setCurrentTodo({...todo});
+}
+function handleUpdateTodo(id,updatedTodo){
+  console.log(id, updatedTodo, "updated todo");
+  const updatedItem = items.map((item) => {
+    return item.id === id ? updatedTodo : item;
+  });
+
+  setIsEditing(false);
+  setItems(updatedItem);
+}
+function handleEditFormSubmit (e) {
+  e.preventDefault();
+  handleUpdateTodo(currentTodo.id, currentTodo);
+
+}
+
   return (
     <div>
-      <div className="task-input">
+        {isEditing ? (
+          <form onSubmit={handleEditFormSubmit}>
+            <h2>Edit ToDo</h2>
+            <label htmlFor="editTodo">Edit Todo:</label>
+           <input
+              name="editTodo"
+              type="text"
+              placeholder="Edit todo"
+              value={currentTodo.value}
+              onChange={handleEditInputChange}
+          />
+           <button type="submit">Update</button>
+         
+          <button onClick={() => setIsEditing(false)}>Cancel</button>
+        </form>
+        ) : (
+          <form onSubmit={addItem}>
+          <h2>Add Todo</h2>
+          <label htmlFor="todo">Add todo: </label>
+         
+          <input
+            name="todo"
+            type="text"
+            placeholder="Create a new todo"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
+          <button onClick={() => addItem()}>Add</button>
+        </form>
+      )}
+      
+      {/* <div className="task-input">
         <i className="uil-align-left"></i>
         <input
           className="text"
@@ -33,13 +97,13 @@ const InputComponent = () => {
           onChange={(e) => setNewItem(e.target.value)}
         ></input>
         <button onClick={() => addItem()}>Add</button>
-      </div>
+      </div> */}
       <div>
         {items.map((item) => (
           <div key={item.id}>
             {item.value}
             <button onClick={() => deleteItem(item.id)}>Delete</button>
-            <button>Edit</button>
+            <button onClick={() => handleEditClick(item)}>Edit</button>
           </div>
         ))}
       </div>
