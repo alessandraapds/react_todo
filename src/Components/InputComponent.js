de52e76fdcb41566462922057ba0e867b7e96e0f
Luis Rojas <luisignacio1210@gmail.com>
2023-01-18 17:57:09 +0100

    adding some functionality

--- src/Components/InputComponent.js
+++ src/Components/InputComponent.js
@@ -6,6 +6,9 @@ const InputComponent = () => {
   // Array that holds all new items
   const [items, setItems] = useState([]);
 
+  // const[isEditing, setIsEditing] =useState(false)
+  // const[currentTodo, setCurrentTodo] =useState({})
+
   function addItem() {
     if (!newItem) {
       alert("Enter new Item");
@@ -17,26 +20,37 @@ const InputComponent = () => {
       id: Math.floor(Math.random() * 1000),
       value: newItem,
     };
+
     setItems((oldList) => [...oldList, item]);
     setNewItem("");
-    console.log(items);
+  }
+
+  function deleteItem(id) {
+     const newArray = items.filter((item) => item.id !== id);
+     setItems(newArray);
   }
 
   return (
     <div>
       <div className="task-input">
-        <i className="uil-align-left"></i>
+        <i ></i>
         <input
           type="ext"
           placeholder="Add new task"
           value={newItem}
           onChange={(e) => setNewItem(e.target.value)}
-        ></input>
+        />
+        
         <button onClick={() => addItem()}>Add</button>
         <>
           <ul>
             {items.map((item) => {
-              return <li key={item.id}>{item.value}</li>;
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button onclick={() => deleteItem(item.id)}> ❌</button>
+                </li>
+              );
             })}
           </ul>
         </>
